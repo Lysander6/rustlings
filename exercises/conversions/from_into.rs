cb6a90cf51c22d3c84d5e81e992490873de74875
Lysander <lysander6@gmail.com>
2022-07-31 18:39:07 +0200

    store old (v4.4.0) solutions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,25 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let parts: Vec<&str> = s.split(',').collect();
+
+        if let [name, age_str] = parts[..] {
+            if name.len() == 0 {
+                return Person::default();
+            }
+            
+            if let Ok(age) = age_str.parse::<usize>() {
+                return Person {
+                    name: name.into(),
+                    age,
+                };
+            }
+        }
+
+        Person::default()
     }
 }
 
